c36bfad0b16c931fcd0b176feeaecf3965ab34bd
mahfuzar07 <mahfuzar271@gmail.com>
2026-03-15 14:59:38 +0600

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import Footer from '../components/layouts/Footer';
 import Navbar from '../components/layouts/Navbar';
-import CTA from '../components/pages-section/home/CTA';
+// import CTA from '../components/pages-section/home/CTA';
 import FeaturedProducts from '../components/pages-section/home/FeaturedProducts';
 import Hero from '../components/pages-section/home/Hero';
-import HowItWorks from '../components/pages-section/home/HowItWorks';
+// import HowItWorks from '../components/pages-section/home/HowItWorks';
 import IndustriesSection from '../components/pages-section/home/IndustriesSection';
-import Services from '../components/pages-section/home/Services';
-import Testimonials from '../components/pages-section/home/Testimonials';
+// import Services from '../components/pages-section/home/Services';
+// import Testimonials from '../components/pages-section/home/Testimonials';
 import WhyChooseUs from '../components/pages-section/home/WhyChooseUs';
 // import TrustBar from '../components/pages-section/home/TrustBar';
 
